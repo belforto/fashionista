43c287458c3ffa79ae935b03f73d2081046c19ae
belforto <marko.ognjenovich@gmail.com>
2022-12-13 19:21:10 +0100

    fix: glowy border

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,10 +1,5 @@
-import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 import style from './card.module.scss';
-import { CardSwiper } from 'react-card-rotate-swiper';
-import axios from 'axios';
-import Image from 'next/image';
 import { clsx } from 'clsx';
 interface CardProps {
 	img: any;
