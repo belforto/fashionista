0299b432982732e6df3549425b160e17c4884216
belforto <marko.ognjenovich@gmail.com>
2022-12-20 23:53:54 +0100

    photo api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import path from 'path';
+const fs = require('fs');
 
 type Data = {
 	name: string;
@@ -10,5 +12,12 @@ export default function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	res.status(200).json({ name: 'John Doe' });
+	const photosDir = path.join(process.cwd(), 'public/photos');
+	console.log('jsonDirectory: ', photosDir);
+	const images = fs
+		.readdirSync(photosDir, { withFileTypes: true })
+		.filter((item: any) => !item.isDirectory())
+		.map((item: any) => '/photos/' + item.name);
+	console.log('images: ', images);
+	return res.status(200).json(images);
 }
